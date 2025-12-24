bc96d4b71f94b5a3affecb7c596cc7428461b8fb
Francesco Pietro Rizzello <francescorizzello94@gmail.com>
2025-12-24 19:18:15 +0100

    [add] snowman favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
-
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link
+		rel="icon"
+		type="image/svg+xml"
+		href="data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20viewBox%3D%220%200%20100%20100%22%3E%3Ctext%20y%3D%22.9em%22%20font-size%3D%2290%22%3E%E2%98%83%EF%B8%8F%3C/text%3E%3C/svg%3E"
+	/>
 </svelte:head>
 
 {@render children()}
